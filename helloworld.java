9a59a9535561696ef785d148e944ec80ea5f7922
Karthik <83061346+karti555@users.noreply.github.com>
2021-05-10 14:15:17 +0530

    5th line added

--- helloworld.java
+++ helloworld.java
@@ -6,6 +6,5 @@ class HelloWorld {
         System.out.println("Hello welcome to all from Kobzo");
         System.out.println("New Line added Demo");
         System.out.println("New Line added");
-        System.out.println("New Line added from arumugasamy");
     }
 }
