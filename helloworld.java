1f7eade30d817aa4015e2cb9bd6b410283d1ba4c
Karthik <83061346+karti555@users.noreply.github.com>
2021-05-10 14:25:20 +0530

    KP-2 #comment this is the demo commit and new line added

--- helloworld.java
+++ helloworld.java
@@ -6,5 +6,6 @@ class HelloWorld {
         System.out.println("Hello welcome to all from Kobzo");
         System.out.println("New Line added Demo");
         System.out.println("New Line added");
+        System.out.println("New Line added");
     }
 }
